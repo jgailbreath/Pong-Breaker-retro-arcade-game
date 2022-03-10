42c129bf16528be8a1224902a696c956d27ac1d9
Codey-Newton <98991499+Codey-Newton@users.noreply.github.com>
2022-03-10 15:14:51 -0600

    brick 3/10/22

---
+++ 2D_core/Assets/Scripts/brickScript.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class brickScript : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer { get; private set; }
+    public Sprite[] states;
+    public int health { get; private set; }
+
+    public void Awake()
+    {
+        this.spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        this.health = this.states.Length;
+        this.spriteRenderer.sprite = this.states[this.health - 1];
+    }
+
+    private void Hit()
+    {
+        this.health--;
+
+        if (this.health <= 0)
+        {
+            this.gameObject.SetActive(false);
+        }
+        else
+        {
+            this.spriteRenderer.sprite = this.states[this.health - 1];
+        }
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ball")
+        {
+            Hit();
+        }
+    }
+}
