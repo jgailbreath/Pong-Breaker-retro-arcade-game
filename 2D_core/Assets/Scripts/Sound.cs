050e2e77824a209ef6900de9878a8250b1102e8c
Jesse Gailbreath <jesse.gailbreath@gmail.com>
2022-04-10 20:45:04 -0500

    added: Audio Manager, additional audio clips, tags
    --created Audio Manager that uses tags for game objects to initialize sounds
    *** this still needs some work to function properly!!!
    --created some new tags for game objects
    --added new audio clips (all are now housed in Audio Manager)

---
+++ 2D_core/Assets/Scripts/Sound.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+[System.Serializable]
+
+public class Sound
+{
+    public string name;
+
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume;
+
+    [Range(0.1f, 2f)]
+    public float pitch;
+
+    public bool loop;
+
+    [HideInInspector]
+    public AudioSource source;
+}
