794099fce00b49be1c9a6496cef6ba25644c3439
Ronnie Jackson <RonnieJ507@gmail.com>
2022-04-18 18:43:53 -0500

    Update Ball and Paddle scripts

--- 2D_core/Assets/Scripts/Paddle.cs
+++ 2D_core/Assets/Scripts/Paddle.cs
@@ -7,7 +7,6 @@ public class Paddle : MonoBehaviour
     public float speed = 6f;
     protected Rigidbody2D rigidBody;
     protected Vector3 orgPos;
-    public gameUIScript UI;
        
 
     private void Awake()
@@ -15,23 +14,11 @@ public class Paddle : MonoBehaviour
         rigidBody = GetComponent<Rigidbody2D>();
     }
 
-    private void Update()
-    {
-        if (UI.GAMEOVER)
-        {
-            rigidBody.constraints = RigidbodyConstraints2D.FreezeAll;
-        }
-    }
-
     void OnCollisionEnter2D(Collision2D collision)
     {
-
         if (collision.gameObject.tag == "Ball")
         {
             rigidBody.velocity = Vector2.zero;
         }
-
-            
-
     }
 }
