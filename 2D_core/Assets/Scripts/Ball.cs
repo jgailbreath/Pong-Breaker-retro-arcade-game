cebc0ba3648496d83d9b3cfe32b4546154f32bfa
Ronnie Jackson <RonnieJ507@gmail.com>
2022-04-20 19:41:45 -0500

    Update movement for the Ball objects

--- 2D_core/Assets/Scripts/Ball.cs
+++ 2D_core/Assets/Scripts/Ball.cs
@@ -2,17 +2,21 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    public float speed = 20f;
+    public float speed = 6f;
     public Transform paddle;
     public float timeVal = 5f;
     public gameUIScript UI;
     private bool onPaddle;
     private Rigidbody2D rigidBody;
-    private Vector2 dir;
+    private Vector2 startVelocity;
     private bool countDown = true;
     private float orgTimeVal;
     public bool reset = false;
-    
+
+    private void Start()
+    {
+        onPaddle = true;
+    }
 
     private void Awake()
     {
@@ -20,11 +24,11 @@ public class Ball : MonoBehaviour
         orgTimeVal = timeVal;
         if (paddle.parent.CompareTag("Player1"))
         {
-            dir = Vector2.right;
+            startVelocity = new Vector2(speed, 0f);
         }
-        else if (paddle.parent.CompareTag("Opponent"))
+        else if (paddle.parent.CompareTag("Player2"))
         {
-            dir = Vector2.left;
+            startVelocity = new Vector2(-1 * speed, 0f);
         }
     }
 
@@ -33,7 +37,7 @@ public class Ball : MonoBehaviour
         rigidBody.velocity = Vector2.zero;
         onPaddle = true;
         countDown = true;
-        timeVal = 5f;
+        timeVal = orgTimeVal;
         reset = false;
     }
 
@@ -44,31 +48,28 @@ public class Ball : MonoBehaviour
             transform.position = paddle.position;
         }
 
-        if (Input.GetButtonDown("Jump") && onPaddle)
-        {
-            rigidBody.AddForce(dir * this.speed);
-            onPaddle = false;
-            timeVal = 0f;
-        }
-
         if (timeVal >= 0 && countDown)
         {
             timeVal -= Time.deltaTime;
         }
         else if (onPaddle)
         {
-            rigidBody.AddForce(dir * this.speed);
+            rigidBody.velocity = startVelocity;
             onPaddle = false;
             countDown = false;
             timeVal = orgTimeVal;
         }
-    }
 
-    private void Start()
-    {
-        onPaddle = true;
-    }
+        if ((rigidBody.velocity.x > -1 * speed) && (rigidBody.velocity.x <= 0f))
+        {
+            rigidBody.velocity = new Vector2(-1 * speed,rigidBody.velocity.y);
+        }
+        else if ((rigidBody.velocity.x < speed) && (rigidBody.velocity.x >= 0f))
+        {
+            rigidBody.velocity = new Vector2(speed, rigidBody.velocity.y);
+        }
 
+    }
     
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -82,13 +83,33 @@ public class Ball : MonoBehaviour
             UI.LoseLife2();
             reset = true;
         }
-        else if (collision.CompareTag("Player"))
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        float velY = rigidBody.velocity.y;
+        if (collision.gameObject.CompareTag("Wall") || collision.gameObject.CompareTag("Opponent"))
         {
-            rigidBody.AddForce(Vector2.right * this.speed);
+            rigidBody.velocity = new Vector2(-1 * this.speed,velY);
         }
-        else if (collision.CompareTag("Opponent"))
+        else if (collision.gameObject.CompareTag("Player1"))
         {
-            rigidBody.AddForce(Vector2.left * this.speed);
+            rigidBody.velocity = new Vector2(this.speed, velY);
+        }
+        else if (collision.gameObject.CompareTag("NorthWall"))
+        {
+            if (velY <= 0)
+            {
+                rigidBody.AddForce(Vector2.down * this.speed);
+            }
+        }
+        else if (collision.gameObject.CompareTag("SouthWall"))
+        {
+            if (velY <= 0)
+            {
+                rigidBody.AddForce(Vector2.up * this.speed);
+            }
         }
     }
 }
