1cb50d7135199eff3cd5edeab0d8441114e53910
Codey-Newton <98991499+Codey-Newton@users.noreply.github.com>
2022-04-25 20:58:46 -0500

    Comments on UI and Brick scripts
    C.N. 4/25/22

--- 2D_core/Assets/Scripts/brickScript.cs
+++ 2D_core/Assets/Scripts/brickScript.cs
@@ -4,38 +4,41 @@ using UnityEngine;
 
 public class brickScript : MonoBehaviour
 {
-    public SpriteRenderer spriteRenderer { get; private set; }
-    public Sprite[] states;
-    public int health { get; private set; }
+    public SpriteRenderer spriteRenderer { get; private set; } // Allows to access sprites
+    public Sprite[] states; // Array to hold sprites for different brick states
+    public int health { get; private set; } // Holds current brick state
 
     public void Awake()
     {
-        this.spriteRenderer = GetComponent<SpriteRenderer>();
+        this.spriteRenderer = GetComponent<SpriteRenderer>(); // Initializing sprite renderer
     }
 
     private void Start()
     {
-        this.health = this.states.Length;
-        this.spriteRenderer.sprite = this.states[this.health - 1];
+        this.health = this.states.Length; // Current health of brick
+        this.spriteRenderer.sprite = this.states[this.health - 1]; // Initializing sprite state
     }
 
+    // Triggers when a brick is hit
     private void Hit()
     {
-        this.health--;
+        this.health--; // Decrement health
 
+        // If brick has 0 health, destroy brick.
         if (this.health <= 0)
         {
             Destroy(this.gameObject);
         }
-        else
+        else // Iterate to next visible brick state
         {
             this.spriteRenderer.sprite = this.states[this.health - 1];
         }
     }
 
-
+    // Determines collisions
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        // If brick detects a collision from the ball, call the Hit function
         if(collision.gameObject.tag == "Ball")
         {
             Hit();
--- 2D_core/Assets/Scripts/gameUIScript.cs
+++ 2D_core/Assets/Scripts/gameUIScript.cs
@@ -7,66 +7,68 @@ using TMPro;
 
 public class gameUIScript : MonoBehaviour
 {
-    private int lives1;
-    private int lives2;
-    public TextMeshProUGUI lifeText1;
-    public TextMeshProUGUI lifeText2;
-    public TextMeshProUGUI time;
-    public GameObject loseObject1;
-    public GameObject loseObject2;
-    public GameObject mainMenuButton;
-    public GameObject playAgainButton;
-    public Button pauseButton;
-    public Ball ball;
-    private float timer;
-    private bool singleMode = false;
-    private bool noBricks = false;
+    private int lives1; // Lives for player 1
+    private int lives2; // Lives for player 2
+    public TextMeshProUGUI lifeText1; // Display for player 1 lives
+    public TextMeshProUGUI lifeText2; // Display for player 2 lives
+    public TextMeshProUGUI time; // Display for timer
+    public GameObject loseObject1; // Displays when player 1 wins
+    public GameObject loseObject2; // Displays when player 2 wins
+    public GameObject mainMenuButton; // Button to take user back to main menu
+    public GameObject playAgainButton; // Button to play level again
+    public Button pauseButton; // Button to pause game
+    public Ball ball; // Takes timer from ball script
+    private float timer; // Variable to hold amount of time left before ball launch
+    private bool singleMode = false; // Checks what game mode user is in
+    private bool noBricks = false; // Checks if any bricks remain
 
 
 
     void Start()
     {
-        Time.timeScale = 1;
-        loseObject1.SetActive(false);
-        loseObject2.SetActive(false);
-        mainMenuButton.SetActive(false);
-        playAgainButton.SetActive(false);
-        lives1 = 3;
+        Time.timeScale = 1; // Make sure game is not paused
+        loseObject1.SetActive(false);     //
+        loseObject2.SetActive(false);     // Set game objects and buttons to not appear prematurely
+        mainMenuButton.SetActive(false);  //
+        playAgainButton.SetActive(false); // 
+        lives1 = 3; // Initialize player 1 and two lives
         lives2 = 3;
-        SetCountText();
-        timer = ball.timeVal + 1;
+        SetCountText(); // Initialize text boxes
+        timer = ball.timeVal + 1; // Initialize timer value
     }
 
     private void Update()
     {
+        // If both players are alive and timer is running, display timer
         if ((lives1 >= 1 && lives2 >= 1) && timer >= 1)
         {
             timer = (int)ball.timeVal + 1;
             time.text = timer.ToString();
         }
-        else
+        else // Remove timer
         {
             time.text = "";
         }
 
-
+        // Do not display out of range number
         if (time.text == "6")
         {
             time.text = "";
         }
 
+        // If game is paused, display menu buttons
         if (Time.timeScale == 0)
         {
             mainMenuButton.SetActive(true);
             playAgainButton.SetActive(true);
         }
-        else
+        else // Hide menu buttons when game is not paused
         {
             mainMenuButton.SetActive(false);
             playAgainButton.SetActive(false);
         }
 
-
+        // Check if single player mode is over
         if (singleMode)
         {
             if (!noBricks && GameObject.FindGameObjectWithTag("Brick") == null)
@@ -77,22 +79,24 @@ public class gameUIScript : MonoBehaviour
         }
     }
 
+    //Initialize player lives textbox
     void SetCountText()
     {
         lifeText1.text = "Lives: " + lives1.ToString();
-        if (SceneManager.GetActiveScene().name == "Scene2")
+        if (SceneManager.GetActiveScene().name == "Scene2") // Check if user is in 2 player mode
         {
             lifeText2.text = "Lives: " + lives2.ToString();
-            FindObjectOfType<AudioManager>().Play("Background_2");
+            FindObjectOfType<AudioManager>().Play("Background_2"); // Play background music for level 2
         }
-        else if (SceneManager.GetActiveScene().name == "Scene1" || SceneManager.GetActiveScene().name == "SinglePlayerDemo")
+        else if (SceneManager.GetActiveScene().name == "Scene1" || SceneManager.GetActiveScene().name == "SinglePlayerDemo") // Check if user is in 1 player mode
         {
             lifeText2.text = "";
             singleMode = true;
-            FindObjectOfType<AudioManager>().Play("Background_3");
+            FindObjectOfType<AudioManager>().Play("Background_3"); // Play background music for level 1
         }
     }
 
+    // Checks if single player is over (victory condition)
     void BrickChecker()
     {
         loseObject2.SetActive(true);
@@ -102,15 +106,20 @@ public class gameUIScript : MonoBehaviour
         FindObjectOfType<AudioManager>().Play("Victory");
     }
 
+    // Removes life from player 1 if a ball hits their goal
     public void LoseLife1()
     {
         lives1--;
         lifeText1.text = "Lives: " + lives1.ToString();
+
+        // Check if player 1 has no more lives and activate end game conditions
         if (lives1 < 1)
         {
             loseObject1.SetActive(true);
             time.text = "";
             pauseButton.onClick.Invoke();
+
+            // Display appropriate components depending on game mode (double or single player)
             if (singleMode)
             {
                 FindObjectOfType<AudioManager>().Stop("Background_3");
@@ -126,10 +135,13 @@ public class gameUIScript : MonoBehaviour
         }
     }
 
+    // Removes life from player 2 if a ball hits their goal
     public void LoseLife2()
     {
         lives2--;
         lifeText2.text = "Lives: " + lives2.ToString();
+
+        // Check if player 2 has no more lives and activate end game conditions
         if (lives2 < 1)
         {
             loseObject2.SetActive(true);
